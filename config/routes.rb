09934c49a58e1f1b5aad7261e5f6fb528c48859d
 NoraBourouis <bourouisnora@gmail.com>
2019-03-04 20:02:09 +0100

    modif des routes pour avoir de belles routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:show, :index]
-  get '/contact', to: 'home#index', as: :contact
+  resources :home, only: [:index], as: :contact
 
 
 end
