97436417f22e2c72771f710a71969cc7ab5a79cd
Marylis <stenger_marylis@hotmail.fr>
2019-03-04 18:20:12 +0100

    footer finalized header wip

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,15 @@ ActiveRecord::Schema.define(version: 2019_03_04_101422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price", precision: 10, scale: 2
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "last_name"
     t.string "first_name"
@@ -27,14 +36,6 @@ ActiveRecord::Schema.define(version: 2019_03_04_101422) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  
-    create_table "items", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.decimal "price", precision: 10, scale: 2
-    t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
