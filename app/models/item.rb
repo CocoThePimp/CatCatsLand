ef9338cdea65d0423f1682f3212de4e8e986db24
Marylis <stenger_marylis@hotmail.fr>
2019-03-04 11:13:34 +0100

    validation and seed

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  validates :image_url, presence: true #, uniqueness: true
+  validates :title, presence: true
+  validates :price, presence: true
+  validates :description, length: { in: 20..100 }
 end
