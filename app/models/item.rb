6bd8a510a63857a01b300cb7401933d0a1045409
Marylis <stenger_marylis@hotmail.fr>
2019-03-05 10:41:31 +0100

    creation table cart and jointablecartitems

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true
   validates :description, length: { in: 20..100 }
+
+  has_many :join_table_cart_items
+  has_many :carts, through: :join_table_cart_items
 end
