8da36909085e05c404b87e3a3f25372e45801bb9
 NoraBourouis <bourouisnora@gmail.com>
2019-03-05 17:56:05 +0100

    ajout des lignes dans model item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many :order_contents
   has_many :items, through: :order_contents
+  has_one_attached :image_url
   validates :image_url, presence: true #, uniqueness: true
   validates :title, presence: true
   validates :price, presence: true
@@ -8,4 +9,10 @@ class Item < ApplicationRecord
 
   has_many :join_table_cart_items
   has_many :carts, through: :join_table_cart_items
+
+def image_url
+  image_url.attach(params[:image_url])
 end
+
+end
+
