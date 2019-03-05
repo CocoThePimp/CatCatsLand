2411d0c6661e9349045a0775fe6235afd7703f40
hnassri <nassrihamdi@gmail.com>
2019-03-05 20:46:40 +0100

    button delete up to date

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:show, :index]
+  resources :items, only: [:show, :index, :destroy]
   resources :home, only: [:index], as: :contact
   resources :carts, except: [:show, :new]
 end
