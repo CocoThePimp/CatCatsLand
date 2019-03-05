309d76e68f60af338ea6dacbced5d9bc4c9a0f39
Maxime Smolis <mpsmolis@gmail.com>
2019-03-05 12:24:15 +0100

    en cours

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'orders/show'
+  get 'orders/create'
   devise_for :users
   root to: "items#index"
   resources :items, only: [:show, :index]
   resources :home, only: [:index], as: :contact
+  resources :order, only: [:show, :create]
 
 
 end
