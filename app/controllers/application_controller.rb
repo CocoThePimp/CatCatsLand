d5dfe7023adbd61307d60fbd04ff7c84018b9a0e
hnassri <nassrihamdi@gmail.com>
2019-03-06 18:58:15 +0100

    devise form work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
  
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.sanitize.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.sanitize.permit(:sign_up, keys: [:last_name])
-    devise_parameter_sanitizer.sanitize.permit(:account_update,  keys: [:first_name])
-    devise_parameter_sanitizer.sanitize.permit(:account_update,  keys:  [:last_name])
+  # private
   
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.sanitize.permit(:sign_up, keys: [:first_name])
+  #   devise_parameter_sanitizer.sanitize.permit(:sign_up, keys: [:last_name])
+  #   devise_parameter_sanitizer.sanitize.permit(:account_update,  keys: [:first_name])
+  #   devise_parameter_sanitizer.sanitize.permit(:account_update,  keys:  [:last_name])
+  
+  # end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
   private
   
+  # def sign_up_params
+  #   params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  # end
+
   def sign_up_params
-    params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    devise_parameter_sanitizer.sanitize(:sign_up)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
+  # def account_update_params
+  #   params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  # end
+
   def account_update_params
-    params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    devise_parameter_sanitizer.sanitize(:account_update)
   end
+
 end
