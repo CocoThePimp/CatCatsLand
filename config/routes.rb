63bb6e0a4c09fe072f1d6d43c06e08390fed6c89
 NoraBourouis <bourouisnora@gmail.com>
2019-03-06 12:05:36 +0100

    ajout de la branche profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :items, only: [:show, :index, :destroy]
   resources :home, only: [:index], as: :contact
   resources :carts, except: [:show, :new]
-  resources :orders, only: [:create]
+  resources :orders, only: [:create, :show]
   resources :charges
 end
