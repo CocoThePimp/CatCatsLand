76445654f13475c0cfacf0e954e92e34c975e9bc
Maxime Smolis <mpsmolis@gmail.com>
2019-03-06 16:38:22 +0100

    ok

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,24 +3,21 @@ class OrdersController < ApplicationController
   
   def create
     
-    @amount = params[:amount] * 100
+    # @amount = params[:amount] * 100
   
-    customer = Stripe::Customer.create({
-      email: params[:stripeEmail],
-      source: params[:stripeToken],
-    })
+    # customer = Stripe::Customer.create({
+    #   email: params[:stripeEmail],
+    #   source: params[:stripeToken],
+    # })
   
-    charge = Stripe::Charge.create({
-      customer: customer.id,
-      amount: @amount,
-      description: 'Rails Stripe customer',
-      currency: 'eur',
-    })
+    # charge = Stripe::Charge.create({
+    #   customer: customer.id,
+    #   amount: @amount,
+    #   description: 'Rails Stripe customer',
+    #   currency: 'eur',
+    # })
   
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
-   
+  
       order_user
   end
     
