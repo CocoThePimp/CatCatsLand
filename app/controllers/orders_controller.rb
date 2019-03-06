3a955561dc134d20a4eae6572f9625b20576005b
Maxime Smolis <mpsmolis@gmail.com>
2019-03-06 19:30:21 +0100

    params dans creation d'order ok

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,13 +5,13 @@ class OrdersController < ApplicationController
     
     @amount = params[:amount].to_i
   
-    customer = Stripe::Customer.create({
+    @customer = Stripe::Customer.create({
       email: params[:stripeEmail],
       source: params[:stripeToken],
     })
   
     charge = Stripe::Charge.create({
-      customer: customer.id,
+      customer: @customer.id,
       amount: @amount * 100,
       description: 'Rails Stripe customer',
       currency: 'eur',
@@ -28,7 +28,7 @@ class OrdersController < ApplicationController
 
   def order_user
     @object = JoinTableCartItem.where(cart: current_user.cart)
-    @order = Order.create(user: current_user)
+    @order = Order.create(user: current_user, stripe_customer_id: @customer.id, amount: params[:amount])
     @object.each do |object|
       OrderContent.create(item_id: object.item_id, order_id: @order.id)
       object.destroy
