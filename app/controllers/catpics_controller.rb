6b1f3062dad033708d7e0cd6b7351f9576ab2760
 NoraBourouis <bourouisnora@gmail.com>
2019-03-07 15:32:26 +0100

    ajout de photo possible via interface admin

--- app/controllers/catpics_controller.rb
+++
@@ -1,7 +0,0 @@
-class CatpicsController < ApplicationController
-  def create
-    @item = Item.find(params[:item_id])
-    @item.catpic.attach(params[:catpic])
-    redirect_to(item_path(@item))
-  end
-end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   has_many :order_contents
   has_many :items, through: :order_contents
-  validates :image_url, presence: true #, uniqueness: true
   validates :title, presence: true
   validates :price, presence: true
   validates :description, length: { in: 20..100 }
