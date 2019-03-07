9c5b21ad232911764220bbe7ee6ab2871eb923bd
Maxime Smolis <mpsmolis@gmail.com>
2019-03-07 18:47:24 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,19 @@ Rails.application.routes.draw do
   # resources :charges
   resources :home, only: [:index], as: :contact
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations' }, path_names: {sign_in: "connection", sign_out: "deconnection"}
 
   root to: "items#index"
 
-  scope(path_names: { index: 'catalogue_des_produits', show: 'ohohohoh_il_est_trop_mimi', destroy: 'oupsy...gone'}) do
+  scope(path_names: { index: 'catalogue_des_produits', show: 'ohohohoh_il_est_trop_mimi', create: 'oupsy'}) do
     resources :items, only: [:index, :show, :create], path: 'photos_de_chatons'
   end
 
-  scope(path_names: { show: 'voici_ton_panier', new: 'nouveau_panier'}) do
+  
     resources :carts, except: [:show, :new], path: 'mon_panier'
-  end
+  
 
-  scope(path_names: { show: 'mes_commandes', new: 'nouvelle_commande'}) do
+  scope(path_names: { show: 'mes_commandes', create: 'nouvelle_commande'}) do
     resources :orders, only: [:create, :show], path: 'mes_commandes'
   end
   
