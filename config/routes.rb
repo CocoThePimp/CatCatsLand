2095600a6bcf6f6863b21a55356c51e56c2ea628
 NoraBourouis <bourouisnora@gmail.com>
2019-03-08 01:03:07 +0100

    routing des routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,14 @@ Rails.application.routes.draw do
   
   # resources :charges
   resources :home, only: [:index], as: :contact
-
-  devise_for :users, :controllers => { registrations: 'registrations' }, path_names: {sign_in: "connection", sign_out: "deconnection", edit: "paramètres"}
-
   root to: "items#index"
+  devise_for :users, :controllers => { registrations: 'registrations' }, path_names: {sign_in: "connexion", sign_out: "déconnexion", edit: "paramètres"}
 
-  scope(path_names: { index: 'catalogue_des_produits', show: 'ohohohoh_il_est_trop_mimi', create: 'oupsy'}) do
-    resources :items, only: [:index, :show, :create]
+  scope(path_names: { index: 'catalogue_des_produits', show: 'detail', create: 'nouvel_article'}) do
+    resources :items, only: [:index, :show, :create], path: 'catalogue_des_produits'
   end
 
-  
-  resources :carts, except: [:show, :new]
-  
+  resources :carts, except: [:show, :new], path: 'panier'
 
   scope(path_names: { show: 'mes_commandes', create: 'nouvelle_commande'}) do
     resources :orders, only: [:create, :show], path: 'mes_commandes'
@@ -26,7 +22,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users , path: 'espace_admin'
     resources :items do
-      resources :catpics, only: [:create], path: 'ajout_photo'
+      resources :catpics, only: [:create]
     end
     resources :orders, path: 'espace_commandes'
   end
